86e573e81b3186a7181b507895eaebb18726f4bc
harshhg <myfolder.pc@gmail.com>
2019-06-09 09:45:01 +0000

    Updated-dynamic year

--- name_age_year.py
+++ name_age_year.py
@@ -1,8 +1,8 @@
 #!/bin/python3
-
+import datetime
 name=input("Your Name : ")
 age=int(input("Your Age : "))
+d=datetime.datetime.now()
 
-
-print("{} you will turn 95 by the year : {}".format(name,(95-age)+2018))
+print("{} you will turn 95 by the year : {}".format(name,(95-age)+d.year))
 
